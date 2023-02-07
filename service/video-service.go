967d334d827a2c7ed055b4d0d4a6efbadfb50a28
abdoohossamm <abdoohossamm@outlook.com>
2023-02-07 04:30:50 +0200

    create the video service and entity

---
+++ service/video-service.go
@@ -0,0 +1,27 @@
+package service
+
+import (
+	"github.com/abdoohossamm/golang-gin-first/entity"
+)
+
+type VideoService interface {
+	Save(video entity.Video) entity.Video
+	FindAll() []entity.Video
+}
+
+type videoService struct {
+	videos []entity.Video
+}
+
+func (service videoService) Save(video entity.Video) entity.Video {
+	service.videos = append(service.videos, video)
+	return video
+}
+
+func (service videoService) FindAll() []entity.Video {
+	return service.videos
+}
+
+func New() VideoService {
+	return &videoService{}
+}
